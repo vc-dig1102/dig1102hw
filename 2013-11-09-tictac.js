68699f7761939231e97c3677366d5b9ac6d1422c
OpenShift guest <csilvestro@mail.valenciacollege.edu>
2013-11-09 12:03:02 -0500

    Make game

---
+++ 2013-11-09-tictac.js
@@ -0,0 +1,106 @@
+//Make a tic tac to game
+
+var x = "x.jpg";
+// Location of where you uploaded your site's x.jpg image
+
+var o = "o.jpg";
+// Location of where you uploaded your site's o.jpg image
+
+var blank = "blank.jpg";
+// Location of where you uploaded your site's blank.jpg image
+
+var pause = 0;
+var all = 0;
+var a = 0;
+var b = 0;
+var c = 0;
+var d = 0;
+var e = 0;
+var f = 0;
+var g = 0;
+var h = 0;
+var i = 0;
+var temp="";
+var ok = 0;
+var cf = 0;
+var choice=9;
+var aRandomNumber = 0;
+var comp = 0;
+var t = 0;
+var wn = 0;
+var ls = 0;
+var ts = 0;
+
+function help() {
+alert("Welcome to Tic-Tac-Toe!  You play as the X's and the computer is the O's.  Select the square you want to put your X into by clicking them.  You cannot occupy a square that is already occupied. The first player to get three squares in a row wins.  Good Luck!!")
+}
+
+
+function logicOne() {
+if ((a==1)&&(b==1)&&(c==1)) all=1;
+
+
+
+
+}
+
+
+
+function logicTwo() {
+
+
+
+}
+
+
+
+function clearOut() {
+
+}
+
+function checkSpace() {
+
+}
+
+
+function yourChoice(chName) {
+
+
+}
+
+
+
+function taken() {
+
+
+}
+
+
+function myChoice() {
+
+
+}
+
+
+
+function ended() {
+
+
+}
+
+
+function process() {
+
+
+}
+
+
+function playAgain() {
+
+
+}
+
+
+function reset() {
+
+}
