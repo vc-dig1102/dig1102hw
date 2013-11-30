2c54705cf9fdcef584d6436e550a75238abde45b
OpenShift guest <csilvestro@mail.valenciacollege.edu>
2013-11-30 16:49:27 -0500

    Help for wk 10 & 11 Homework

---
+++ 2013-11-30-Wk10-11-hw.js
@@ -0,0 +1,26 @@
+List at least three functions.
+Document them in a new file called homework-7.1.js
+Identify the name of the function and its arguments with comments.
+Identify the in-scope variables by name; document any anomalies.
+Identify the return value of each function.
+
+Example:
+
+function myFunction()
+{
+alert("Hello World!");
+}
+
+//or
+
+function myFunc(theObject)
+ {
+   theObject.brand = "Toyota";
+ }
+ 
+ function myFunction()
+{
+var x=5;
+return x;
+}
+
