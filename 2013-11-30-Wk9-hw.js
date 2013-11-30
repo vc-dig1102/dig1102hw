80880f5811ec7a2a73c17d39984676dae93c59c9
OpenShift guest <csilvestro@mail.valenciacollege.edu>
2013-11-30 15:43:49 -0500

    Homework for week 9 update

---
+++ 2013-11-30-Wk9-hw.js
@@ -0,0 +1,27 @@
+Wk9 hw:  Looping and branching code: at least five branches that you can diagram.
+
+Copy-paste your example into "homework-6.1.md" and attempt to identify the loop conditions with comments e.g.
+
+
+while ( count < 5 ) {
+
+// while `count` is less than 5
+
+
+collection.forEach(function(item){
+// until there are no more `items` in the `collection`
+
+
+//The following are examples.  Just give me five different examples of this code written out.
+
+Example:
+
+for (var key in object) {
+    //do stuff with object[key]
+}
+
+
+var animals = ['dog', 'cat', 'bear'];
+animals.forEach(function(animal, index) {
+    alert(index + ':' + animal); // '0:dog', '1:cat', '2:bear'
+});
